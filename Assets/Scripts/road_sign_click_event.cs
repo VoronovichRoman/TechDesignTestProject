af5116cbff826779c2349316287c5a4a90a8344c
VoronovichRoman <voronovich.roman@gmail.com>
2023-10-25 00:14:06 +0300

    добавил анимацию чата, спрайт с камнями; подправил анимации
    1. Добавил префаб и анимацию чата с продавцом магазина.
    2. Добавил статичные спрайты камней. Они будут использоваться для украшения и как триггеры.
    3. Изменил скорость анимаций сундука и дорожного знака.

--- Assets/Scripts/road_sign_click_event.cs
+++ Assets/Scripts/road_sign_click_event.cs
@@ -6,9 +6,15 @@ public class road_sign_click_event : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private GameObject _nextSceneButton;
     [SerializeField] private GameObject _hero;
+    private Animator _animator;
     public void OnPointerClick(PointerEventData eventData)
     {
+        _animator.SetTrigger("isResize");
         _hero.GetComponent<hero>().Move(this.transform.position.x);
+    }
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- Assets/Scripts/shop_click_event.cs
+++ Assets/Scripts/shop_click_event.cs
@@ -5,8 +5,19 @@ using UnityEngine.EventSystems;
 public class shop_click_event : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private GameObject _hero;
+    [SerializeField] private GameObject _chatBubble;
+    [SerializeField] private GameObject _road_sign;
     public void OnPointerClick(PointerEventData eventData)
-    {
+    {        
         _hero.GetComponent<hero>().Move(-1);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _road_sign.GetComponent<Animator>().SetTrigger("isResize");
+        _chatBubble.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _chatBubble.SetActive(false);
     }
 }
